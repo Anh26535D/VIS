fd77790357cbdfde2b5240853409af7f3939fba4
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2023-05-25 21:56:58 +0700

    add irbank crawler

---
+++ 2023-05/IrbankCrawler/IrCrawlHelper.py
@@ -0,0 +1,104 @@
+import requests as r
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+
+def getValidCodes(c_code="E01244"):
+    link = f"https://irbank.net/{c_code}/reports"
+    rs = r.get(link)
+    rsp = BeautifulSoup(rs.content, 'html.parser')
+
+    table = rsp.find("table")
+    stock_slice_batch = pd.read_html(str(table),extract_links='all')[0]
+    list_block = table.find_all("td")
+    dict_ = {}
+    for block in list_block:
+        list_a = block.find_all("a")
+        try:
+            title = "".join([i.string for i in list_a])
+            link_basic = list_a[0]["href"]
+            link = list_a[-1]["href"]
+            key = f'{link_basic}'
+            dict_[key] = link
+        except:
+            pass
+
+    for col in stock_slice_batch.columns[1:5]:
+        for row in stock_slice_batch.index:
+            t = stock_slice_batch[col][row]
+            for key in dict_.keys():
+                if t[1] == key:
+                    stock_slice_batch[col][row] = dict_[key]
+
+    fy_report_codes = stock_slice_batch[('通期', None)]
+    result = []
+    for code in fy_report_codes:
+        if code[-2:] == 'pl' and code[:-3] not in result:
+            result.append(code[:-3])
+
+    return result
+
+def get_data(table):
+    data = []
+    h = []
+    rows = table.find_elements(By.XPATH, ".//tbody/tr")
+    headers = rows[0].find_elements(By.XPATH, ".//th")
+    if len(headers) > 2:
+        for header in headers[0::2]:
+            value = header.text.replace('\n', ' ')              
+            h.append(value)      
+    else:
+        for header in headers:
+            value = header.text.replace('\n', ' ')              
+            h.append(value)
+    data.append(h)
+
+    mem = []
+    prev_check = 0
+    for row in rows[1:]:
+        r = []
+        cols = row.find_elements(By.XPATH, ".//td")
+        cls = row.get_attribute('class')
+        if cls == 'row3':
+            if prev_check == 1:
+                mem[-1] = mem[-1] + "__" + cols[0].text.replace('\n', ' ')
+            else:
+                mem.append(cols[0].text.replace('\n', ' '))
+            prev_check = 1
+        else:
+            prev_check = 0
+            value = cols[0].text.replace('\n', ' ')  
+            if len(mem)>0:
+                value = str(mem[-1]) + "__" + value            
+            r.append(value)
+
+            value = cols[-1].text.replace('\n', ' ')            
+            r.append(value)
+            data.append(r)
+    return pd.DataFrame(data)    
+
+def concat_data(dfs):
+    v_index_val = []
+    h_index_val = []
+    for df in dfs:
+        for v in df.iloc[:, 0]:
+            if v not in v_index_val:
+                v_index_val.append(v)
+        for v in df.loc[0]:
+            if v not in h_index_val:
+                h_index_val.append(v)
+    h_index_val = h_index_val[1:]
+    dict_data = {k: [] for k in v_index_val}
+    for df in dfs:
+        for k in dict_data.keys():
+            temp_df = df.iloc[:, 0]
+            if k in temp_df.tolist():
+                idx = 0
+                for value in temp_df:
+                    if value == k:
+                        break
+                    idx += 1
+                dict_data[k].append(df.iloc[idx, -1])
+            else:
+                dict_data[k].append("")
+    return dict_data
---
+++ 2023-05/IrbankCrawler/sample_irhelper.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import edge
+from time import sleep
+import csv
+from IrCrawlHelper import *
+
+options = edge.options.Options()
+options.add_argument("--headless=new")
+driver = webdriver.Edge(options=options)
+
+REPORT_TYPE = "pl"
+C_CODE = "E00015"
+
+codes = getValidCodes(C_CODE)
+
+dfs = []
+
+for code in codes:
+    link = f"https://irbank.net/{C_CODE}/{code}/{REPORT_TYPE}"
+    print(link)
+    driver.get(link)
+    sleep(1)
+
+    table = driver.find_element(By.ID, f"c_{REPORT_TYPE}1")
+    data = get_data(table)
+    dfs.append(data)
+print("DONE!!!")
+data = concat_data(dfs)
+driver.quit()
+
+with open(f'data_{C_CODE}_{REPORT_TYPE}.csv', 'w', newline='', encoding='utf-8') as csvfile: 
+    csvwriter = csv.writer(csvfile)  
+    csvwriter.writerows([[k] + v for k, v in data.items()])
